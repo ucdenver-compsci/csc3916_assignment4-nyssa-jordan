e48007a657cef76b51ec86224acdc2008cff6ebc
nyssa-jordan <jordannyssa@gmail.com>
2024-04-10 20:13:13 -0600

    updates shcema

--- Movies.js
+++ Movies.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.DB)
 .then(
   MovieSchema = new mongoose.Schema({
     title: { type: String, required: true, index: true },
-    releaseDate: Date,
+    releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
     genre: {
       type: String,
       enum: [
@@ -17,9 +17,11 @@ mongoose.connect(process.env.DB)
       actorName: String,
       characterName: String,
     }],
+    imageUrl: String
   })
 
 );
+
 
 // return the model
 module.exports = mongoose.model('Movie', MovieSchema);
